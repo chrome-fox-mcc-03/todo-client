2724db481a8781396a4dbd9d2d7c5ab4de1d166b
alvinarkansas <alvinarkansas@gmail.com>
2020-03-04 18:19:37 +0700

    created done, edit ongoing

--- index.js
+++ index.js
@@ -1,12 +1,52 @@
+function fetchTodos() {
+    $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3000/todos',
+        headers: {
+            token: localStorage.getItem('token')
+        } 
+    })
+        .done(todos => {
+            $('#todos-space').empty();
+            for (let i = 0; i < todos.length; i++) {
+                $('#todos-space').append(`<p>${todos[i].title}</p><button onclick="editTodo(${todos[i].id})">Edit</button>`)
+            }
+            console.log(todos);
+        })
+        .fail(err => {
+            console.log(err);
+        })
+}
+function editTodo(id){
+    // alert(id)
+    $.ajax({
+        method: 'GET',
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token: localStorage.getItem('token')
+        } 
+    })
+        .done(todoFound => {
+            console.log(todoFound);
+            $('#update-title').val(todoFound.title)
+        })
+        .fail(err => {
+            console.log('error!', err);
+        })
+
+}
 $(document).ready(function() {
     if (localStorage.getItem('token')) {
+        fetchTodos();
         $('#dashboard-page').show();
         $('#signup-page').hide();
         $('#signin-page').hide();
+        $('#create-todo-page').hide();
     } else {
         $('#dashboard-page').hide();
         $('#signup-page').hide();
         $('#signin-page').show();
+        $('#create-todo-page').hide();
     }
 
     $('#signup-form').on('submit', function(e) {
@@ -21,8 +61,13 @@ $(document).ready(function() {
                 password
             }
         })
-            .done(result => {
-                console.log('sign up success', result);
+            .done(token => {
+                fetchTodos();
+                $('#dashboard-page').show();
+                $('#signup-page').hide();
+                $('#signin-page').hide();
+                $('#create-todo-page').hide();
+                console.log('sign up success', token);
             })
             .fail(err => {
                 console.log('sign up failed', err);
@@ -44,9 +89,11 @@ $(document).ready(function() {
             .done(token => {
                 localStorage.setItem('token', token);
                 console.log('sign in success', token);
+                fetchTodos();
                 $('#dashboard-page').show();
                 $('#signup-page').hide();
                 $('#signin-page').hide();
+                $('#create-todo-page').hide();
             })
             .fail(err => {
                 console.log('sign in failed', err);
@@ -55,25 +102,66 @@ $(document).ready(function() {
 
     $('#btn-signout').on('click', function() {
         localStorage.clear();
-        $('dashboard-page').hide();
-        $('#signup-form').show();
-        $('#signin-form').show();
+        $('#dashboard-page').hide();
+        $('#signup-page').hide();
+        $('#signin-page').show();
+        $('#create-todo-page').hide();
     })
 
     $('#btn-redir-signup').on('click', function() {
         $('#dashboard-page').hide();
         $('#signup-page').show();
         $('#signin-page').hide();
+        $('#create-todo-page').hide();
     })
 
     $('#btn-redir-signin').on('click', function() {
         $('#dashboard-page').hide();
         $('#signup-page').hide();
         $('#signin-page').show();
+        $('#create-todo-page').hide();
     })
 
-    // $('#btn').on('click', function() {
-    //     $('#dashboard-page').hide();
-    // })
-    // $('#landing-page').show();
+    $('#btn-redir-create-todo').on('click', function() {
+        $('#dashboard-page').hide();
+        $('#signup-page').hide();
+        $('#signin-page').hide();
+        $('#create-todo-page').show();
+    })
+
+    $('#create-todo-form').on('submit', function(e) {
+        e.preventDefault();
+        const title = $('#title').val();
+        const description = $('#description').val();
+        const due_date = $('#due_date').val();
+        // console.log(title, description, status, due_date);
+        // console.log(localStorage.getItem('token'));
+        $.ajax({
+            method: 'POST',
+            url: 'http://localhost:3000/todos',
+            data: {
+                title,
+                description,
+                status: false,
+                due_date
+            },
+            headers: {
+                token: localStorage.getItem('token')
+            }
+        })        
+            .done(createdTodo => {
+                console.log('New todo successfully created', createdTodo);
+                $('#dashboard-page').show();
+                $('#signup-page').hide();
+                $('#signin-page').hide();
+                $('#create-todo-page').hide();
+            })
+            .fail(err => {
+                console.log('Error!', err);
+            })
+    })
+
+    $('#btn-edit-todo22').on('click', function() {
+        console.log('editin dong');
+    })
 })
