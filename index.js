3e1525df5fc4b346dc9d0b50471d0b175098e29a
maxhamz <maxwell.hamzah@gmail.com>
2020-03-04 23:46:32 +0700

    google signin icon added. pending googe authorization at backend & create-update-delete todos

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 let message
 let token
+let title
+let description
+let status
+let due_date
+let email
+let password
+let userId
+let todoId
 
-// $( document ).ready(function() {
+// $( document ).ready(function() {})
     token = localStorage.getItem('token')
     if(token) {
         renderDashboard()
@@ -18,10 +26,14 @@ let token
         renderSignIn()
     })
 
+    $("#btn-logout").on('click', function() {
+        localStorage.clear()
+        renderSignIn()
+    })
+
 
     // $("#signin-form").on("click")
 
-// });
 
 function renderDashboard() {
     $("#signin-page").hide()
@@ -48,13 +60,11 @@ function renderSignIn() {
     $("#dashboard-page").hide()
 }
 
-
-
 function login(event) {
     event.preventDefault()
 
-    const email = $("#signin-email").val()
-    const password = $("#signin-password").val()
+    email = $("#signin-email").val()
+    password = $("#signin-password").val()
 
     console.log(`Welcome Back to Todos App, ${email}`);
 
@@ -67,55 +77,29 @@ function login(event) {
         }
     })
     .done(response => {
+
+
         console.log(`Register success, now generating token`);
         console.log(`response is`);
         console.log(response);
         localStorage.setItem('token', response.token)
-        //show dashboard, hide login
-        // showTodos()
-        // renderDashboard()
-
-        // token = localStorage.getItem('token')
-
-        $.ajax({
-            method: "get",
-            url: "http://localhost:4000/todos/",
-            headers: {
-                token: localStorage.getItem("token")
-            }
-        })
-        .done(response => {
-            console.log(`login success. showing todos`);
-            console.log(response);
-            console.log(`todos layer 1`);
-            console.log(response.todos);
-
-            response.todos.forEach(el => {
-                $("tbl-todo-data").append(`
-                    <tr>
-                        <td>${el.id}</td>
-                        <td>${el.title}</td>
-                        <td>${el.description}</td>
-                        <td>${el.status}</td>
-                        <td>${el.due_date}</td>
-                        <td>DEfault</td>
-                    </tr>
-                `)
-            })
-
-        })
-        
+       
+        showTodos(event)
         renderDashboard()
-        
+
     })
-    .fail(err => {
+    .fail((err, msg) => {
         console.log(err);
-        $(".remarks").append(`<h3>${err}</h3>`)
+        err.responseText = JSON.parse(err.responseText)
+        $(".remarks").append(`<h3>${err.responseText.message}</h3>`)
     })
+
 
 }
 
-/* function showTodos() {
+function showTodos(event) {
+    event.preventDefault()
+    $("#tbl-todo-data").empty()
     token = localStorage.getItem('token')
     $.ajax({
         method: "get",
@@ -131,31 +115,34 @@ function login(event) {
 
         
         response.todos.forEach(el => {
-            $("tbl-todo-data").append(`
-                <tr>
-                    <td>${el.id}</td>
-                    <td>${el.title}</td>
-                    <td>${el.description}</td>
-                    <td>${el.status}</td>
-                    <td>${el.due_date}</td>
-                    <td>DEfault</td>
-                </tr>
-            `)
-        })
+                $("#tbl-todo-data").append(`
+                    <tr>
+                        <td>${el.id}</td>
+                        <td>${el.title}</td>
+                        <td>${el.description}</td>
+                        <td>${el.status}</td>
+                        <td>${el.due_date}</td>
+                        <td>DEfault</td>
+                    </tr>
+                `)
+            })
          
         
     })
-    // .fail(err => {
-    //     showMessage(err.response.JSON.message)
-    // })
+    .fail((err, msg) => {
+        console.log(err);
+        err.responseText = JSON.parse(err.responseText)
+        $(".remarks").append(`<h3>${err.responseText.message}</h3>`)
+    })
+
 
 }
- */
+
 function register(event) {
         event.preventDefault()
        
-        const email = $("#signup-email").val()
-        const password = $("#signup-password").val()
+        email = $("#signup-email").val()
+        password = $("#signup-password").val()
 
         console.log(`Welcome to Todos App, ${email}`);
 
@@ -181,10 +168,48 @@ function register(event) {
             renderSignIn()
 
         })
-        .fail(err => {
+        .fail((err, msg) => {
             console.log(err);
-            $(".remarks").append(`<h3>${err}</h3>`)
+            err.responseText = JSON.parse(err.responseText)
+            $(".remarks").append(`<h3>${err.responseText.message}</h3>`)
         })
 
 }
 
+function createTodo(event) {
+    event.preventDefault()
+    token = localStorage.getItem('token')
+    
+    console.log(`title to be created`);
+
+    title = $("#create-todo-title").val()
+    description = $("#create-todo-description").val()
+    status = $(".create-todo-status").val()
+    due_date = $("#create-todo-due_date").val()
+
+    console.log(`testing`);
+    console.log(`${title} ${description} ${status} ${due_date}`);
+    
+    $.ajax({
+        method: "post",
+        url: "http://localhost:4000/todos/",
+        headers: {
+            token:token
+        },
+        data: {
+            title:title,
+            description:description,
+            status:status,
+            due_date:due_date
+        }
+    })
+    .done(response => {
+        console.log(response);
+    })
+    .fail((err, msg) => {
+        console.log(err);
+        err.responseText = JSON.parse(err.responseText)
+        $(".remarks").append(`<h3>${err.responseText.message}</h3>`)
+    })
+
+}
