4d3f09e56b6a2e795950cac75ba78e4493b7651b
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-06 01:53:46 +0700

    add google login

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 function showLandingPage() {
+    $("#landing-page").show()
     $("#section-register").hide()
     $("#section-login").hide()
     $("#dashboard-page").hide()
-    $("#landing-page").show()
+    $("#button-logout").hide()
+   
+    
 }
 
 function showRegister() {
@@ -24,11 +27,13 @@ function showDashboard() {
     $("#section-login").hide()
     $("#dashboard-page").show()
     $("#landing-page").hide()
+    $("#button-logout").show()
 }
 
 function isLogin() {
     if (localStorage.getItem('token')) {
         return true
+        
     } else {
         return false
     }
@@ -66,20 +71,52 @@ function fetchTodos() {
     })
 }
 
+function onSignIn(googleUser) {
+    // var profile = googleUser.getBasicProfile();
+    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    // console.log('Name: ' + profile.getName());
+    // console.log('Image URL: ' + profile.getImageUrl());
+    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+
+    var id_token = googleUser.getAuthResponse().id_token
+    // console.log(id_token)
+    $.ajax({
+        url: 'http://localhost:3000/users/googleLogin',
+        method: 'POST',
+        data: { id_token }
+    })
+        .done(({ token }) => {
+            localStorage.token = token
+            showDashboard(true)
+            fetchTodos()
+        })
+        .fail(err => {
+            console.log(err)
+        })
+}
+
 $(document).ready(function () {
     showLandingPage()
     const token = localStorage.getItem("token")
 
     $("#button-register").on("click", function () {
         showRegister()
-    })
-
-    $("#button-login").on("click", function () {
-        showLogin()
+        $("#button-logout").hide()
+        
     })
 
     $("#button-home").on("click", function () {
         showLandingPage()
+        $("#button-register").show()
+        $("#button-login").show()
+       
+       
+    })
+
+    $("#button-login").on("click", function () {
+        showLogin()
+        $("#button-logout").hide()
+
     })
 
     $("#form-signup").on("submit", function (e) {
@@ -121,7 +158,14 @@ $(document).ready(function () {
     })
 
     $("#button-logout").on("click", function () {
+        console.log("logout")
+        const auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            console.log('User signed out.');
+        });
         localStorage.clear()
         showLandingPage()
+        $("#button-register").show()
+        $("#button-login").show()
     })
 })
