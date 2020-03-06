5aaa222d58cdf955aa1785b405d614a4a9af1886
Yoko <baghram0001@gmail.com>
2020-03-07 01:14:30 +0700

    adjusted client page,added css

--- index.js
+++ index.js
@@ -1,89 +1,59 @@
+// const isLogin = localStorage.getItem('access_token')
+
 function onSignIn(googleUser) {
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log(id_token)
     $("#LoginButton").hide()
-   $.ajax({
-       url:'http://localhost:3000/user/googlelogin',
-       method:"POST",
-       headers:{
-           access_token: id_token
-       }
-
-   })
-   .done(function(result) {
-       console.log(result)
+    emptyTable()
+    $.ajax({
+        url:'http://localhost:3000/user/googlelogin',
+        method:"POST",
+        headers:{
+            access_token: id_token
+        }
+    })
+    .done(function(result) {
+        console.log(result)
+        localStorage.setItem("access_token", result.access_token)
+        if(isLogin) {
+        $("#dashboard").show()
+        AjaxDashboard()
+    }
     })
     .fail(function(err) {
         console.log(err)
     })
+    
 }
 function signOut() {
     var auth2 = gapi.auth2.getAuthInstance();
     auth2.signOut().then(function () {
     });
+    localStorage.removeItem('access_token')
+    $("#dashboard").hide()
+
 }
 //Google Sign In Sign Out
 
 $(document).ready(function() {
     Start()
+    emptyTable()
+    getDate()
+    $("#LogoutButton").hide()
+
     $('#updates').hide()
-    const isLogin = localStorage.getItem('access_token')
-    console.log(isLogin)
+
+    
+    //IF ALREADY LOGIN GENERATE DASHBOARD
+    let isLogin = localStorage.getItem('access_token')
     if(isLogin) {
-        $("#dashboard").show()
-        $.ajax({
-            url:"http://localhost:3000/todos",
-            method: "GET",
-            headers: {
-                access_token: isLogin
-            }
-        })
-            .done(function(result) {
-                console.log('hahaha')
-                for(let i = 0;i < result.length;i ++) {
-                    $("#todos").append(`
-                    <tr>
-                        <td>${result[i].id}</td>
-                        <td>${result[i].Title}</td>
-                        <td>${result[i].Status}</td>
-                        <td>${result[i].Due_Date}</td>
-                        <td><button class="delete" name="delete" value="${result[i].id}">delete</button></td>
-                        <td><button class="update" name="update" value="${result[i].id}">update</button></td>
-                    </tr>`)
+        LoginSuccess()
+        AjaxDashboard()
+        $("#LogoutButton").show()
 
-                }
-                
-                $(".delete").on("click", function() {
-                    let currentId = this.value
-                    $.ajax({
-                        url:`http://localhost:3000/todos/${currentId}`,
-                        method:"DELETE",
-                        headers:{
-                            access_token: localStorage.getItem('access_token')
-                        },
-                        data: {
-                            id: currentId
-                        }
-                    })
-                    .done(function(result) {
-                        console.log("successfully delete")
-                    })
-                    .fail(function(err) {
-                        console.log(err)
-                    })
-                })
-                //UPDATE BELUM JADI(KERJAKAN BESOK
-                $(".update").on('click', function() {
-                    console.log(this.value)
-                    $("#updates").show()
-                })
 
-                // $()
-            })
-            .fail(function(err) {
-                console.log(err)
-            })
     }
+    //START DASHBOARD DONE
+
    RegisterButton()
    LoginButton()
    LogoutButton()
@@ -98,74 +68,67 @@ $(document).ready(function() {
             }
         })
             .done(function(response) {
+                console.log(response)
                RegisterSuccess()
 
             })
             .fail(function(err) {
                 console.log(err)
+                $("#RegisterFail").append(
+                    `${err.responseJSON[0]}`
+                )
+                setTimeout(() => {
+                    $("#RegisterFail").empty()
+                }, 3000);
             })
     })
 
     $("#form-Login").on('submit', function(e) {
         e.preventDefault()
-        console.log(Email, Password)
         $.ajax({
             url:"http://localhost:3000/user/login",
             method: "POST",
+            
             data: {
                 Email: $("#EmailLogin").val(),
                 Password: $("#passwordLogin").val()
             }
         })
             .done(function(result) {
-                LoginSuccess()
-                console.log(result)
-                console.log('login')
                 localStorage.setItem("access_token", result.access_token)
-                $.ajax({
-                    url:"http://localhost:3000/todos",
-                    method: "GET",
-                    headers: {
-                        access_token: result.access_token
-                    }
-                })
-                    .done(function(result) {
-                        console.log(result)
-                        for(let i = 0;i < result.length;i ++) {
-                            $("#todos").append(`
-                                <tr>
-                                    <td>${result[i].id}</td>
-                                    <td>${result[i].Title}</td>
-                                    <td>${result[i].Status}</td>
-                                    <td>${result[i].Due_Date}</td>
-                                    <td><button class="delete" name="delete" value="${result[i].id}">delete</button></td>
-                                    <td><button class="update" name="update" value="${result[i].id}">update</button></td>
-                                </tr>`)
-                        }
-                        $(".delete").on("click", function() {
-                            let currentId = this.value
-                            $.ajax({
-                                url:`http://localhost:3000/${currentId}`,
-                                method:"DELETE",
-                                data: {
-                                    id: currentId
-                                }
-                            })
-                            .done(function(result) {
-                                console.log("successfully delete")
-                            })
-                            .fail(function(err) {
-                                console.log(err)
-                            })
-                        })
-                    })
-                    .fail(function(err) {
-                        console.log(err)
-                    })
+                let isLogin = localStorage.getItem('access_token')
+                console.log(isLogin)
+                if(isLogin == undefined) {
+                    $("#LoginFail").append(
+                        `Wrong Username / Password`
+                    )
+                    LoginFailed()
+                }
+                else {
+                    LoginSuccess()
+                    console.log(result)
+                    console.log('done form login')
+                    emptyTable()
+                    AjaxDashboard()
+                    
+                }
             })
             .fail(function(err) {
                 console.log(err)
+                $("#LoginFail").append(
+                    `${err.responseJSON.name}`
+                )
             })
+    })
+
+    $("#CreateButton").on("click", function() {
+        $("#creates").show()
+    })
+
+    $("#Create-Form").on('submit', function(e) {
+        e.preventDefault()
+        let isLogin = localStorage.getItem('access_token')
+        CreateTodo(isLogin)
     })
 
 
--- nav.js
+++ nav.js
@@ -2,16 +2,24 @@ function Start() {
     $("#Register").hide()
     $("#Login").hide()
     $("#dashboard").hide()
+    $("#creates").hide()
+    emptyTable()
 }
 
 function RegisterSuccess() {
     $("#Register").hide()
     $("#Login").show()
+    
 }
 
 function LoginSuccess() {
     $("#Login").hide()
     $("#dashboard").show()
+    $("#LoginButton").hide()
+    $("#googlelogin").hide()
+    $("#LogoutButton").show()
+
+
 }
 
 function RegisterButton() {
@@ -30,12 +38,211 @@ function LoginButton() {
 
     })
 }
+
+function emptyTable() {
+    $('#todos').empty()
+    $('#todos').append(`<tr>
+    <th>ID</th>
+    <th>Title</th>
+    <th>Description</th>
+    <th>Status</th>
+    <th>Due Date</th>
+    <th colspan="2">Actions</th>
+</tr>`)
+}
+
 function LogoutButton() {
     $('#LogoutButton').on('click', function() {
         $("#Register").hide()
         $("#Login").hide()
         $("#dashboard").hide()
-        localStorage.removeItem('access_token')
+        $("#LoginButton").show()
+        $("#googlelogin").show()
+        $("#LogoutButton").hide()
 
+        emptyTable()
+        localStorage.removeItem('access_token')
+        signOut()
+    })
+}
+
+function UpdateData(CurrentId) {
+    let isLogin = localStorage.getItem('access_token')
+    $.ajax({
+        url:`http://localhost:3000/todos/${CurrentId}`,
+        method: "PUT",
+        headers: {
+            access_token: isLogin
+        },
+        data: {
+            Title: $("#UpdateTitle").val(),
+            Description: $("#UpdateDesc").val(),
+            Status: $("#UpdateStatus").val(),
+            Due_Date: $("#UpdateDate").val()
+        }
+    })
+        .done(function(result) {
+            $("#Message").append(`Succesfully Updated`)
+            setTimeout(() => {
+                $("#Message").empty()
+            }, 3000);
+            console.log(result)
+            $("#updates").hide()
+            emptyTable()
+            AjaxDashboard()
+        })
+        .fail(function(err) {
+            $("#UpdateError").append(`${err.responseJSON[0]}`)
+            setTimeout(() => {
+                $("#UpdateError").empty()
+            }, 3000);
+        })
+}
+
+function destroy(currentId) {
+    $.ajax({
+        url:`http://localhost:3000/todos/${currentId}`,
+        method:"DELETE",
+        headers:{
+            access_token: localStorage.getItem('access_token')
+        },
+        data: {
+            id: currentId
+        }
+    })
+        .done(function(result) {
+            console.log("successfully delete")
+            console.log(result)
+            emptyTable()
+            AjaxDashboard()
+            
+        })
+        .fail(function(err) {
+            console.log(err)
+        })
+    }
+
+function AjaxDashboard() {
+    let isLogin = localStorage.getItem('access_token')
+    $.ajax({
+        url:"http://localhost:3000/todos",
+        method: "GET",
+        headers: {
+            access_token: isLogin
+        }
+    })
+        .done(function(result) {
+            for(let i = 0;i < result.length;i ++) {
+                $("#todos").append(`
+                <tr>
+                    <td>${result[i].id}</td>
+                    <td>${result[i].Title}</td>
+                    <td>${result[i].Description}</td>
+                    <td>${result[i].Status}</td>
+                    <td>${result[i].Due_Date}</td>
+                    <td><button class="delete" name="delete" value="${result[i].id}">delete</button></td>
+                    <td><button class="update" name="update" value="${result[i].id}">update</button></td>
+                </tr>`)
+
+            }
+            
+            $(".delete").on("click", function() {
+                let currentId = this.value;
+                destroy(currentId)
+            })
+
+            $(".update").on('click', function() {
+                $("#dashboard").hide()
+                let currentId = this.value
+                console.log(this.value)
+                $("#updates").show()
+                $("#Update-Form").on('submit', function (e) {
+                    e.preventDefault()
+                    UpdateData(currentId)
+                })
+            })
+
+        })
+        .fail(function(err) {
+            console.log('error euy')
+            console.log(err)
+        })
+}
+
+function LoginFailed() {
+    $("#Register").hide()
+    $("#Login").hide()
+    $("#dashboard").hide()
+    $("#LoginButton").show()
+    $("#googlelogin").show()
+    emptyTable()
+}
+
+function append(result) {
+    emptyTable()
+    for(let i = 0;i < result.length;i ++) {
+        $("#todos").append(`
+        <tr>
+            <td>${result[i].id}</td>
+            <td>${result[i].Title}</td>
+            <td>${result[i].Description}<td>
+            <td>${result[i].Status}</td>
+            <td>${result[i].Due_Date}</td>
+            <td><button class="delete" name="delete" value="${result[i].id}">delete</button></td>
+            <td><button class="update" name="update" value="${result[i].id}">update</button></td>
+        </tr>`)
+
+    }
+}
+
+
+function getDate() {
+    $.ajax({
+        url: "http://worldclockapi.com/api/json/utc/now",
+        method: "GET"
+    })
+        .done(function(result) {
+            let StringDate = ''
+            for(let i = 0; i < 10; i++) {
+                StringDate+= result.currentDateTime[i]
+            }
+            $("#clock").append(`Today is ${result.dayOfTheWeek}, ${StringDate}`)
+        })
+        .fail(function(err) {
+            console.log(err)
+        })
+}
+
+function CreateTodo(isLogin) {
+    $.ajax({
+        url:`http://localhost:3000/todos/`,
+        method: "POST",
+        headers: {
+            access_token: isLogin
+        },
+        data: {
+            Title: $("#CreateTitle").val(),
+            Description: $("#CreateDesc").val(),
+            Status: $("#CreateStatus").val(),
+            Due_Date: $("#CreateDate").val()
+        }
+    })
+    .done(function(result) {
+        $("#Message").append('Successfully Created')
+        setTimeout(() => {
+            $("#Message").empty()
+        }, 3000);
+        $("#creates").hide()
+        emptyTable()
+        AjaxDashboard()
+
+    })
+    .fail(function(err) {
+        $("#CreateError").append(`${err.responseJSON[0]}`)
+        setTimeout(() => {
+            $("#CreateError").empty()
+
+        }, 3000);
+        console.log(err)
     })
 }
