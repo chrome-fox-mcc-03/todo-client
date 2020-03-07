e33f9250fa9404ba53489c52d5774d97ceaada18
andreasanggaraa <andreasanggaraa@gmail.com>
2020-03-07 13:38:40 +0700

    update local to git

--- index.js
+++ index.js
@@ -14,15 +14,17 @@ $(document).ready(function() {
         $("#login-page").show()
     }
 
-
+    $('.datetimepicker1').datetimepicker({
+        language: 'pt-BR'
+    });
 
 })
 
 function login(event) {
     event.preventDefault()
+    $("#errorMessage-login").empty()
     const email = $("#email-login").val();
     const password = $("#password-login").val();
-    $("#errorMessage").empty()
 
     $.ajax({
         method: "POST",
@@ -117,6 +119,10 @@ function getTodos(token) {
                     <td>${element.description}</td>
                     <td>${element.status}</td>
                     <td>${moment(element.due_date).format('LL')}</td>
+                    <td>
+                    <button onclick="updateTodoLanding(event, ${element.id})" type="button" data-toggle="modal" data-target="#updateTodo-class" class="btn btn-primary">Update</button>                    
+                    <button onclick="deleteTodo(event, ${element.id})" type="button" class="btn btn-danger">Delete</button>
+                    </td>
                 </tr>
                 `)
             })
@@ -144,15 +150,143 @@ function onSignIn(googleUser) {
         }
     })
         .done(function(response) {
-            console.log(response, "clg dari done ajax");
+            getTodos(response)
             $("#dashboard-page").show()
             $("#register-page").hide()
             $("#login-page").hide()
-            getTodos(response)
+            localStorage.setItem('token', response)
         })
         .fail(function(err, msg) {
             err.responseText = JSON.parse(err.responseText)
             $("#errorMessage-login").append(err.responseText.message)
         })
 
-  }
+}
+
+function createTodo(event) {
+    event.preventDefault()
+    $("#errorMessage-addTodo").empty()
+    const title = $("#todo-title").val();
+    const description = $("#todo-description").val();
+    const due_date = $("#todo-due_date").val();
+    let token = localStorage.getItem('token') 
+    $.ajax({
+        method: "POST",
+        url: "http://localhost:3000/todos",
+        headers: {
+            token  
+        },
+        data: {
+            title,
+            description,
+            due_date
+        }
+    })
+        .done(response => {
+            getTodos(token)
+            $("#dashboard-page").show()
+            $("#register-page").hide()
+            $("#login-page").hide()
+
+        })
+        .fail(err => {
+            err.responseText = JSON.parse(err.responseText)
+            $("#errorMessage-addTodo").append(err.responseText.message[0])
+        })
+}
+
+function deleteTodo(event, id) {
+    event.preventDefault()
+    let token = localStorage.getItem('token')
+    $.ajax({
+        method: "DELETE",
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token
+        }
+    })
+        .done(response => {
+            getTodos(token)
+            $("#dashboard-page").show()
+            $("#register-page").hide()
+            $("#login-page").hide()
+        })
+        .fail(err => {
+            console.log(err);
+        })
+}
+
+function updateTodo(event, id) {
+    event.preventDefault()
+    let token = localStorage.getItem('token')
+    let title = $("#update-todo-title").val()
+    let description = $("#update-todo-description").val()
+    let due_date = $("#update-todo-due_date").val()
+    let status = $("#update-todo-status").val()
+
+    $.ajax({
+        method: "PUT",
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token
+        },
+        data: {
+            title,
+            description,
+            due_date,
+            status
+        }
+    })
+        .done(response => {
+            getTodos(token)
+            $("#dashboard-page").show()
+            $("#register-page").hide()
+            $("#login-page").hide()
+            $("#updateTodo-class").modal('hide')
+        })
+
+        .fail(err => {
+            console.log(err);
+        })
+}
+
+function updateTodoLanding(event, id) {
+    event.preventDefault()
+    $("#updateTodo-page").show()
+    $("#updateTodo").attr("onsubmit", `updateTodo(event, ${id})`)
+
+    let token = localStorage.getItem('token')
+    $.ajax({
+        method: "GET",
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token
+        }
+    })
+        .done(response => {
+            $("#update-todo-title").val(response.todo.title)
+            $("#update-todo-description").val(response.todo.description)
+            $("#update-todo-due_date").val(response.todo.due_date)
+            if(response.todo.status) {
+                $("#status-true").attr("selected", "")
+            }
+            else {
+                $("#status-false").attr("selected", "")
+            }
+        })
+        .fail(err => {
+            console.log(err);
+        })
+}
+
+function addTodoLanding(event) {
+    event.preventDefault()
+    // $("#addTodo-page").show()
+    $("#addTodo-class").modal('show')
+}
+
+
+/* 
+Add confirmation on delete
+Check Update todonya
+*/  
