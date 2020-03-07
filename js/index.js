92b22a90820ee95a540a8b2601184f9fe97a8bf8
reinfajar <tsanimfajar@outlook.com>
2020-03-07 15:12:31 +0700

    InsyaaAllah Done  Changes to be committed: 	modified:   css/index.css 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,18 @@
+let settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://api.adviceslip.com/advice",
+	"method": "GET",
+}
+
+$.ajax(settings).done(function (response) {
+    response = JSON.parse(response)
+    $(".quotes").append(`
+            <h1 style="color: red;"> Random Quotes : </h1>
+            <h1> ${response.slip.advice} </h3>
+            `)
+});
+
 function fetchData() {
     $.ajax({
         method: "GET",
@@ -5,95 +20,208 @@ function fetchData() {
         headers: {
             token: localStorage.getItem("token")
         }
-    }).done((data)=>{
+    }).done((response) => {
         $("#listdata").empty()
-        data.forEach(el => {
+        response.data.forEach(el => {
             $("#listdata").append(`
             <tr>
                 <th>${el.title}</th>
                 <th>${el.description}</th>
                 <th>${el.status === true ? "Done" : "Undone"}</th>
                 <th>${new Date(el.due_date).toDateString()}</th>
-                <th><button class="btn btn-secondary" ><a onclick="edit(${el.id})"  value=${el.id}>Edit</a></button> <button class="btn btn-danger" ><a onclick="destroy(${el.id})" value=${el.id}>Delete</a></button> <button class="btn btn-success" ><a onclick="complete(${el.id})"  value=${el.id}>Complete</a</button>
+                <th><button id="edit" class="btn btn-secondary" data-toggle="modal" data-target="#exampleModal">
+                <a onclick="findOne(${el.id})"  value=${el.id}>Edit</a></button> <button class="btn btn-danger" ><a onclick="destroy(${el.id})" value=${el.id}>Delete</a></button> <button class="btn btn-success" ${el.status === true ? "disabled" : ""} ><a onclick="complete(${el.id})" ${el.status === true ? "disabled" : ""}  value=${el.id}>Complete</a</button>
             </tr>
             `)
         });
-    }).fail(function(err, msg){
+    }).fail(function (err, msg) {
         console.log(msg);
     })
 }
-function destroy(id){
-    console.log(id)
+function findOne(id) {
     const token = localStorage.getItem("token")
     $.ajax({
-        method : "delete",
-        url : `http://localhost:3000/todos/${id}`,
+        method: "get",
+        url: `http://localhost:3000/todos/${id}`,
         headers: {
             token
         }
-    }).done((data)=> {
+    }).done((data) => {
+        let el = data.data
+            $("#modalEdit").append(`
+                <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                    <div class="modal-dialog" role="document">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Edit</h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                    </button>
+                            </div>
+                            <div class="modal-body">
+                                <form class="form-group">
+                                    <label for="todo">Todo Name</label><br>
+                                    <input class="form-control input-sm value" id="title_update" type="text" placeholder="Whats You Gonna Do" value="${el.title}"/><br>
+                                    <label for="description">Description</label><br>
+                                    <input class="form-control input-sm value" id="description_update" type="text" placeholder="Description" value="${el.description}"/><br>
+                                    <label for="due_date">Due Date</label><br>
+                                    <input class="form-control input-sm value" id="due_date_update" type="date"/>
+                                </form>
+                            </div>
+                        <div class="modal-footer">
+                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            <button type="button" class="btn btn-primary" onclick="update(${el.id})" >Save changes</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+                `)
+    }).fail((err) => {
+        console.log(err, "error")
+    })
+}
+function update(id){
+    const title = $("#title_update").val()
+    const description = $("#description_update").val()
+    const due_date = $("#due_date_update").val()
+    const token = localStorage.getItem("token")
+    $.ajax({
+        method: "PUT",
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token
+        },
+        data: {
+            title,
+            description,
+            due_date
+        }
+    }).done((data) => {
+        $(".value").val(null) //! disini error jadi seudah update value untuk modals selanjutnya ga bisa dimasukin
         fetchData()
-    }).fail((err)=> {
+    }).fail((err, msg) => {
+        console.log(msg);
+    })
+}
+function complete(id){
+    const token = localStorage.getItem("token")
+    $.ajax({
+        method: "PATCH",
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token
+        }
+    }).done((data) => {
+        Swal.fire(
+            'Good job!',
+            'success'
+          )
+        fetchData()
+    }).fail((err, msg) => {
+        console.log(msg);
+    })
+}
+function destroy(id) {
+    const token = localStorage.getItem("token")
+    $.ajax({
+        method: "delete",
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token
+        }
+    }).done((data) => {
+        fetchData()
+    }).fail((err) => {
         console.log(err, "error")
     })
 }
 
-$(document).ready(function(){
-    if(localStorage.getItem("token")) {
+function onSignIn(googleUser) {
+    const token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+        method: 'post',
+        url: 'http://localhost:3000/google',
+        headers: {
+            token
+        }
+    }).done((data) => {
+        localStorage.setItem("token", data.token)
         $("#dashboard-page").show()
         $("#landing-page").hide()
-        fetchData()
-    } else {
-        $("#dashboard-page").hide()
-        $("#landing-page").show()
-    }
+    }).fail((err) => {
+        console.log(err, "error")
+    })
+}
+
+if (localStorage.getItem("token")) {
+    $("#dashboard-page").show()
+    $("#landing-page").hide()
+    fetchData()
+} else {
+    $("#dashboard-page").hide()
+    $("#landing-page").show()
+}
+$(document).ready(function () {
     $('[data-youtube]').youtube_background({
         fitToBackground: true
     });
-    $('.render a').click(function(){
-        $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
+    $('.render a').click(function () {
+        $('form').animate({
+            height: "toggle",
+            opacity: "toggle"
+        }, "slow");
     });
-    
-    $('.register-form').on("submit", (e)=> {
+
+    $('.register-form').on("submit", (e) => {
         e.preventDefault()
         const email = $(".email").val()
         const password = $(".password").val()
         $.ajax({
-            method : "POST",
-            url : "http://localhost:3000/register",
-            data : {
+            method: "POST",
+            url: "http://localhost:3000/register",
+            data: {
                 email,
                 password
             }
-        }).done((data)=> {
+        }).done((data) => {
             localStorage.setItem("token", data.token)
             $("#dashboard-page").show()
             $("#landing-page").hide()
-        }).fail((err)=> {
-            console.log(err, "error")
+        }).fail((err) => {
+            error = JSON.parse(err.responseText)
+            error.forEach(el => {
+                $(".errorRegister").append(`
+                    <li style="color: red;list-style-type:none">${el}</li>
+                `)
+            });
         })
     })
-    
-    $('.login-form').on("submit", (e)=> {
+
+
+    $('.login-form').on("submit", (e) => {
         e.preventDefault()
         const email = $(".emailLogin").val()
         const password = $(".passwordLogin").val()
         $.ajax({
-            method : "POST",
-            url : "http://localhost:3000/login",
-            data : {
+            method: "POST",
+            url: "http://localhost:3000/login",
+            data: {
                 email,
                 password
             }
-        }).done((data)=> {
-            localStorage.setItem("token", data.token) 
+        }).done((data) => {
+            localStorage.setItem("token", data.token)
             $("#dashboard-page").show()
             $("#landing-page").hide()
-        }).fail((err)=> {
-            console.log(err, "error")
+            $(".errorLogin").empty()
+        }).fail((err) => {
+            error = JSON.parse(err.responseText)
+                $(".errorLogin").append(`
+                    <li style="color: red;list-style-type:none">${error}</li>
+                `)
         })
     })
-    $("#create").on("submit", function(e){
+    $("#create").on("submit", function (e) {
         e.preventDefault()
         const title = $("#title").val()
         const description = $("#description").val()
@@ -112,15 +240,28 @@ $(document).ready(function(){
                 description,
                 status,
                 due_date
-            } 
-        }).done((data)=> {
+            }
+        }).done((data) => {
+            $(".error").empty()
             fetchData()
             console.log(data);
-        }).fail((err, msg)=>{
-            console.log(msg);
+        }).fail((err, msg) => {
+            error = JSON.parse(err.responseText)
+            error.forEach(el => {
+                $(".error").append(`
+                    <li style="color: red;list-style-type:none">${el}</li>
+                `)
+            });
         })
     })
-    $("#logout").on("click", function(){
+    $("#logout").on("click", function () {
+        let auth2 = gapi.auth2.getAuthInstance();
+        auth2.signOut().then(function () {
+            Swal.fire(
+                'You Are Signed Out',
+                'success'
+              )
+        });
         localStorage.clear()
         $("#dashboard-page").hide()
         $("#landing-page").show()
