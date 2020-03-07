6855876d1f166026dd209640256d5739ee1fe3ee
ramadesi1 <ramadesy.saragih@gmail.com>
2020-03-07 17:58:21 +0700

    Add crud todo

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ function showLandingPage() {
     $("#section-register").hide()
     $("#section-login").hide()
     $("#dashboard-page").hide()
-    $("#button-logout").hide()
-   
-    
+    $("#add-todo").hide()
+    $('#update-todo').hide()
 }
 
 function showRegister() {
@@ -27,17 +26,54 @@ function showDashboard() {
     $("#section-login").hide()
     $("#dashboard-page").show()
     $("#landing-page").hide()
-    $("#button-logout").show()
 }
 
 function isLogin() {
     if (localStorage.getItem('token')) {
         return true
-        
     } else {
         return false
     }
 }
+
+function finishTodo(id) {
+    $.ajax({
+        type: 'PATCH',
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token: localStorage.getItem("token")
+        },
+        data: {
+            status: true
+        }
+    })
+        .done(result => {
+            fetchTodos()
+        })
+        .fail(err => {
+            console.log(err)
+        })
+}
+
+function unfinishTodo(id) {
+    $.ajax({
+        type: 'PATCH',
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token: localStorage.getItem("token")
+        },
+        data: {
+            status: false
+        }
+    })
+        .done(result => {
+            fetchTodos()
+        })
+        .fail(err => {
+            console.log(err)
+        })
+}
+
 function fetchTodos() {
     $.ajax({
         method: "GET",
@@ -49,37 +85,62 @@ function fetchTodos() {
         $('#table-todo').empty()
         $('#table-todo').append(`
                 <tr>
+                    <th>No</th>
                     <th>Title</th>
                     <th>Description</th>
                     <th>Status</th>
                     <th>Due Date</th>
+                    <th></th>
+
                 </tr>
             `)
-        response.forEach(el => {
+        response.forEach((el, i) => {
             const due_date = new Date(el.due_date).toLocaleDateString()
             $('#table-todo').append(`
                     <tr>
+                        <td>${i + 1}</td>
                         <td>${el.title}</td>
                         <td>${el.description}</td>
                         <td>${el.status === true ? 'complete' : 'uncomplete'}</td>
                         <td>${due_date}</td>
+                        <td>
+                            ${
+                el.status
+                    ? `
+                                    <a onclick="finishTodo(${el.id})" href="#">
+                                        <span class="icon has-text-success ">
+                                        <i class="fas fa-check"></i>
+                                        </span>
+                                    </a>`
+                    : `
+                                    <a onclick="unfinishTodo(${el.id})" href="#">
+                                        <span class="icon has-text-warning">
+                                        <i class="fas fa-check"></i>
+                                        </span>
+                                    </a> 
+                                `
+                }
+                            <a onclick="showFormUpdate(${el.id})" href="#">
+                                <span class="icon has-text-info">
+                                    <i class="fas fa-edit"></i>
+                                </span>
+                            </a>
+                            <a onclick="deleteTodo(${el.id})" href="#">
+                                <span class="icon has-text-danger">
+                                    <i class="fas fa-trash-alt"></i>
+                                </span>
+                            </a>
+                        </td>
                     </tr>
                 `)
         })
-    }).fail(function (err, msg) {
-        console.log(msg);
+    }).fail(function (err) {
+        console.log(err);
     })
 }
 
 function onSignIn(googleUser) {
-    // var profile = googleUser.getBasicProfile();
-    // console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
-    // console.log('Name: ' + profile.getName());
-    // console.log('Image URL: ' + profile.getImageUrl());
-    // console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
-
     var id_token = googleUser.getAuthResponse().id_token
-    // console.log(id_token)
     $.ajax({
         url: 'http://localhost:3000/users/googleLogin',
         method: 'POST',
@@ -95,28 +156,80 @@ function onSignIn(googleUser) {
         })
 }
 
+function buttonWhenLogin() {
+    $("#button-register").hide()
+    $("#button-login").hide()
+    $("#button-logout").show()
+    $("#button-todo").show()
+
+}
+
+function buttonWhenLogout() {
+    $("#button-register").show()
+    $("#button-login").show()
+    $("#button-logout").hide()
+    $("#button-todo").hide()
+
+}
+
+function showFormAdd() {
+    $('#add-todo').show()
+}
+
+function hideFormAdd() {
+    $('#add-todo').hide()
+}
+
+function showFormUpdate(id) {
+    $('#update-todo').show()
+    $.ajax({
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token: localStorage.getItem("token")
+        },
+        method: 'GET'
+    })
+        .done(response => {
+            const { id, title, description, due_date } = response
+            const date = new Date(due_date).toISOString().split('T')[0]
+
+            $('#title-update').val(title)
+            $('#description-update').val(description)
+            $('#due_date-update').val(date)
+        })
+        .fail(err => {
+            console.log(err)
+        })
+}
+
+function hideFormUpdate() {
+    $('#update-todo').hide()
+}
+
+
 $(document).ready(function () {
-    showLandingPage()
-    const token = localStorage.getItem("token")
+    if (isLogin()) {
+        showLandingPage()
+        buttonWhenLogin()
+    } else {
+        showLogin()
+        buttonWhenLogout()
+    }
 
     $("#button-register").on("click", function () {
         showRegister()
-        $("#button-logout").hide()
-        
     })
 
     $("#button-home").on("click", function () {
         showLandingPage()
-        $("#button-register").show()
-        $("#button-login").show()
-       
-       
     })
 
     $("#button-login").on("click", function () {
         showLogin()
-        $("#button-logout").hide()
-
+    })
+    $("#button-todo").on("click", function () {
+        showDashboard()
+        fetchTodos()
     })
 
     $("#form-signup").on("submit", function (e) {
@@ -134,9 +247,40 @@ $(document).ready(function () {
             localStorage.setItem("token", response)
             showDashboard()
             fetchTodos()
+            buttonWhenLogin()
         }).fail(function (err) {
             console.log(err, "error")
         })
+    })
+
+    $("#add-todo").on("submit", function(e) {
+        e.preventDefault()
+        const title = $('#title').val()
+        const description = $('#description').val()
+        const due_date = $('#due_date').val()
+        let data = {
+            title,
+            description,
+            due_date,
+            status: false
+        }
+
+        $.ajax({
+            method: 'POST',
+            url: 'http://localhost:3000/todos',
+            headers: {
+                token: localStorage.getItem('token')
+            },
+            data
+        })
+            .done(response => {
+                console.log(response)
+                fetchTodos()
+                hideFormAdd()
+            })
+            .fail(err => {
+                console.log(err)
+            })
     })
 
     $("#form-login").on("submit", function (e) {
@@ -154,18 +298,19 @@ $(document).ready(function () {
             localStorage.setItem("token", response)
             showDashboard()
             fetchTodos()
+            buttonWhenLogin()
         })
     })
 
+    
+
     $("#button-logout").on("click", function () {
-        console.log("logout")
         const auth2 = gapi.auth2.getAuthInstance();
         auth2.signOut().then(function () {
             console.log('User signed out.');
         });
         localStorage.clear()
         showLandingPage()
-        $("#button-register").show()
-        $("#button-login").show()
+        buttonWhenLogout()
     })
 })
