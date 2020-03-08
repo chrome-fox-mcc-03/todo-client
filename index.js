c5b3977d8200f55d855920bdf7f6f50172c7ed4b
thomasmartuaps <thomasmartuaps@gmail.com>
2020-03-08 14:18:29 +0700

    Edit done, design basically done

--- index.js
+++ index.js
@@ -11,25 +11,111 @@ function showDashboard() {
         
     }).done(function(response) {
         $('#list-item').empty()
-        response.forEach(el => $('#list-item').append(`
+        response.forEach(el => {
+        
+        $('#list-item').append(`
         <section>
+        <br>
         <div class="text">
             <p>
             <h5><b>${el.title}</b></h5>
             ${el.description}<br>
             <b>Done:</b> ${el.status}<br>
             <b>Due on: </b> ${el.due_date}<br><br>
-            <button type="button" class="edit-or-delete" data-toggle="modal" data-target="#editToDo" onClick="editToDo(${el.id}, ['${el.title}', '${el.description}', '${el.status}', '${el.due_date}'])">Edit</button> | 
-            <button type="button" class="edit-or-delete" onClick="deleteToDo(${el.id}, '${el.title}')">Delete</button></p><br>
+            <button type="button" class="edit-or-delete" onClick="showToDo(${el.id})">Edit</button> | 
+            <button type="button" class="edit-or-delete" onClick="deleteToDo(${el.id}, '${el.title}')">Delete</button>
+            </p>
         </div>
         <section>
-            `))
+        <form id="edit-todo-${el.id}">
+                        <div class="edit-form">
+                            <label for="title-edit-${el.id}">Title:</label><br>
+                            <input class="input-one-line"  type="text" id="title-edit-${el.id}" value="${el.title}"><br>
+                            <label for="description-edit-${el.id}">Description:</label><br>
+                            <textarea form="edit-todo" id="description-edit-${el.id}" cols="35" placeholder="Describe this activity!">${el.description}</textarea><br>
+                            <label for="status-edit-${el.id}">Done?</label><br>
+                            <select class="due-date" id="status-edit-${el.id}">
+                            <option value=true>true</option>
+                            <option value=false>false</option>
+                            </select><br>
+                            <label for="due_date-edit-${el.id}">Due Date:</label><br>
+                            <input class="input-one-line" type="text" id="due_date-edit-${el.id}" value="${el.due_date}" placeholder="Please use YYYY-MM-DD!"><br><br>
+                            <button type="button" class="pinkbutton" onClick="edit(${el.id})">Post</button>
+                            <button type="button" class="edit-or-delete" onClick="hideToDo(${el.id})">Cancel</button>
+                        </div>
+                    </form>
+            `)
+        hideToDo(el.id)
+        // submitEdit(el.id)
+        })
             // console.log(response)
     })
     .fail(function(err) {
         console.log(err, " <= It's an error.")
     })
 }
+
+function showToDo(id) {
+    $(`#edit-todo-${id}`).show()
+}
+
+function hideToDo(id) {
+    $(`#edit-todo-${id}`).hide()
+}
+
+function submitEdit(id) {
+    let title = $(`#title-edit-${id}`).val()
+    let truth = ($(`#status-edit-${id}`).val() == "true")
+    $(`#edit-todo-${id}`).submit(function() {
+        $.ajax({
+            method: "PUT",
+            url: `http://localhost:3000/todos/${id}`,
+            headers: {
+                token: localStorage.getItem('token')
+            },
+            data: {
+                title: $(`#title-edit-${id}`).val(),
+                description: $(`#description-edit-${id}`).val(),
+                status: truth,
+                due_date: $(`#due_date-edit-${id}`).val()
+            }
+
+        }).done(response => {
+            console.log(response)
+            console.log(`Updated item "${title}"`)
+            showDashboard()
+        })
+        .fail(err => console.log(err))
+    })
+}
+
+function edit(id) {
+    console.log(id)
+    let title = $(`#title-edit-${id}`).val()
+    let truth = ($(`#status-edit-${id}`).val() == "true")
+    console.log({title: title, test: truth})
+    $.ajax({
+        method: "PUT",
+        url: `http://localhost:3000/todos/${id}`,
+        headers: {
+            token: localStorage.getItem('token')
+        },
+        data: {
+            title: $(`#title-edit-${id}`).val(),
+            description: $(`#description-edit-${id}`).val(),
+            status: truth,
+            due_date: $(`#due_date-edit-${id}`).val()
+        }
+    }).done(response => {
+        console.log(response)
+        console.log(`Updated item "${title}"`)
+        showDashboard()
+        alert(`Updated item "${title}"`)
+    })
+    .fail(err => console.log(err))
+    hideToDo(id)
+}
+
 function addButton() {
     $("#add-todo").submit(function() {
         $.ajax({
@@ -69,27 +155,6 @@ function editToDo(id, array) {
     console.log(id)
 }
 
-function edit(id) {
-    console.log
-    $.ajax({
-        method: "PUT",
-        url: `http://localhost:3000/todos/${id}`,
-        headers: {
-            token: localStorage.getItem('token')
-        },
-        data: {
-            title: $("#title-edit").val(),
-            description: $("#description-edit").val(),
-            status: $("#status-edit").val(),
-            due_date: $("#due_date-edit").val()
-        }
-
-    })
-}
-// function addToDo() {
-//     $
-// }
-
 function deleteToDo(id, title) {
     $.ajax({
         method: "DELETE",
@@ -111,16 +176,16 @@ function deleteToDo(id, title) {
 function onSignIn(googleUser) {
     // Useful data for your client-side scripts:
     var profile = googleUser.getBasicProfile();
-    console.log("ID: " + profile.getId()); // Don't send this directly to your server!
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
+    // console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+    // console.log('Full Name: ' + profile.getName());
+    // console.log('Given Name: ' + profile.getGivenName());
+    // console.log('Family Name: ' + profile.getFamilyName());
+    // console.log("Image URL: " + profile.getImageUrl());
+    // console.log("Email: " + profile.getEmail());
 
     // The ID token you need to pass to your backend:
     var id_token = googleUser.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
+    // console.log("ID Token: " + id_token);
 
     $.ajax({
         method: "POST",
