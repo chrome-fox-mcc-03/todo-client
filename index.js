593708308e9db2519c23d83ea7ab4ec3a3ba67da
ulfahasanah <ulfa2912@outlook.com>
2020-03-11 15:19:50 +0700

    revision done all

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const baseURL = "https://still-forest-80696.herokuapp.com"
 let id_update;
 
+
 function landingPage() {
     $("#menu").show()
     $("#navbar").show()
@@ -16,6 +17,7 @@ function landingPage() {
     $("#update-form").hide()
     $("#dashboard-button").hide()
     $("#logout").hide()
+    $(".loading").hide()
 }
 
 function showRegister() {
@@ -33,6 +35,7 @@ function showRegister() {
     $("#update-form").hide()
     $("#dashboard-button").hide()
     $("#logout").hide()
+    $(".loading").hide()
 }
 
 function showLogin() {
@@ -50,6 +53,7 @@ function showLogin() {
     $("#update-form").hide()
     $("#dashboard-button").hide()
     $("#logout").hide()
+    $(".loading").hide()
 }
 
 
@@ -69,6 +73,7 @@ function dashboard() {
     $("#update-form").hide()
     $("#dashboard-button").show()
     $("#logout").show()
+    $(".loading").hide()
 }
 
 function showCreateForm() {
@@ -87,6 +92,7 @@ function showCreateForm() {
     $("#update-form").hide()
     $("#dashboard-button").show()
     $("#logout").show()
+    $(".loading").hide()
 }
 
 function showUpdateForm() {
@@ -105,10 +111,16 @@ function showUpdateForm() {
     $("#update-form").show()
     $("#dashboard-button").show()
     $("#logout").show()
+    $(".loading").hide()
 }
 
+function showLoading() {
+    $(".loading").show()
+}
+
+
 function signUp(event) {
-    event.preventDefault() // untuk menghindari reload ketika on submit
+    event.preventDefault()
     const email = $("#email").val()
     const password = $("#password").val()
     $.ajax({
@@ -121,14 +133,18 @@ function signUp(event) {
     })
     .done(function(response){
         localStorage.setItem("token", response.token)
-        console.log(response);
         dashboard()
-
+        showTodo()
+        $("#password").val("")
+        $("#email").val("")
     })
-    .fail(function(err){
-        console.log(err);
-        $("#errorSignUp").empty()
-        $("#errorSignUp").append(`<p class="text-danger"> ${err.responseJSON.error} </p>`)
+    .fail(function(err){        
+        $("#errorSignUp").empty()                
+        err.responseJSON.error.forEach( el => {
+            $("#errorSignUp").append(`<p class="text-danger"> ${el} </p>`)
+        })
+        $("#password").val("")
+        $("#email").val("")
     })
 }
 
@@ -136,7 +152,6 @@ function login(event) {
     event.preventDefault()
     const email = $("#emailLogin").val() 
     const password = $("#passLogin").val()
-    
     $.ajax({
         method: "POST",
         url: `${baseURL}/login`,
@@ -147,14 +162,16 @@ function login(event) {
     })
     .done(function(response) {
         localStorage.setItem("token", response.token)
-        console.log(response);
         dashboard()
         showTodo()
-        
+        $("#emailLogin").val("") 
+        $("#passLogin").val("") 
     })
-    .fail(function(err) {
+    .fail(function(err) { 
         $("#errorPass").empty()
         $("#errorPass").append(`<p class="text-danger"> ${err.responseJSON.error} </p>`)
+        $("#emailLogin").val("") 
+        $("#passLogin").val("") 
     })
 }
 
@@ -170,13 +187,14 @@ function showTodo() {
         if(response){
             $("#tbody-list").empty()
             response.data.forEach(list => {
+                list.status === false ? list.status = "pending" : list.status = "done"
                 $('#tbody-list').append(`
                 <tr>
                         <td>${list.title}</td>
                         <td>${list.description}</td>
                         <td>${list.status}</td>
                         <td>${new Date(list.due_date).toDateString()}</td>
-                        <td> <a href="${list.countdown}"> link countdown </a> </td>
+                        <td> <a href="${list.countdown}" target="_blank"> link countdown </a> </td>
                         <td> <button class="btn btn-success" onclick="findOne(${list.id})">Edit</button> 
                              <button class="btn btn-danger" onclick="deleteTodo(${list.id})">Delete</button>
                         </td>
@@ -186,16 +204,42 @@ function showTodo() {
         }
     })
     .fail(function(err) {
-        console.log(err)
     })
 }
 
+function onSignIn(googleUser) {
+    const token = googleUser.getAuthResponse().id_token;
+    $.ajax({
+            method : 'POST',
+            url: `${baseURL}/signInGoogle`,
+            headers: {
+                token: token
+            }
+        })
+        .done(result => {
+            localStorage.setItem("token", result.token)
+            dashboard()
+        })
+        .fail(err => {
+        })
+}
+
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+    });
+  }
+
+
 function create(event) {
     event.preventDefault()
+    $("#errorUpdate").empty()
+    $("#errorCreate").empty()
     const title = $("#title-create").val()
     const description = $("#description-create").val()
     const due_date = $("#due_date-create").val()
     const status = false
+    showLoading()
     $.ajax({
         method: "POST",
         url: `${baseURL}/todos`,
@@ -210,14 +254,21 @@ function create(event) {
         }
     })
         .done(response => {
-            console.log(response);
             dashboard()
             showTodo()
+            $("#title-create").val("")
+            $("#description-create").val("")
+            $("#due_date-create").val("")
         })
-        .fail(err => {
-            console.log(err)
+        .fail(err => {  
+            $(".loading").hide()          
             $("#errorCreate").empty()
-            $("#errorCreate").append(`<p class="text-danger"> All input must be filled <br> Due date at least is tomorrow </p>`)
+            err.responseJSON.error.forEach( el => {
+                $("#errorCreate").append(`<p class="text-danger"> ${el} </p>`)
+            })
+            $("#title-create").val("")
+            $("#description-create").val("")
+            $("#due_date-create").val("")
         })
 }
 
@@ -234,11 +285,12 @@ function deleteTodo(id) {
             showTodo()
         })
         .fail(err => {
-            console.log(err)
         })
 }
 
 function findOne(id) {
+    $("#errorUpdate").empty()
+    $("#errorCreate").empty()
     id_update = id
     $.ajax({
         method: "GET",
@@ -248,9 +300,9 @@ function findOne(id) {
         }
     })
         .done(response => {
-            console.log(response);
             const dateVal = new Date(response.data.due_date)
             response.data.due_date = `${dateVal.getFullYear()}-${dateVal.getMonth()+1 < 10 ? "0" + (dateVal.getMonth()+1) : dateVal.getMonth()+1}-${dateVal.getDate() < 10 ? "0"+dateVal.getDate() : dateVal.getDate()}`
+            response.data.status === false ? response.data.status = "pending" : response.data.status = "done"
             showUpdateForm()
             $("#title-update").val(response.data.title)
             $("#description-update").val(response.data.description)
@@ -258,7 +310,6 @@ function findOne(id) {
             $("#due_date-update").val(response.data.due_date)
         })
         .fail(err => {
-            console.log(err);
         })
 }
 
@@ -268,6 +319,7 @@ function update(event) {
     const description = $("#description-update").val()
     const due_date = $("#due_date-update").val()
     const status = $("#status-update").val()
+    showLoading()
     $.ajax({
         method: "PUT",
         url: `${baseURL}/todos/${id_update}`,
@@ -284,11 +336,17 @@ function update(event) {
         .done(response => {
             dashboard()
             showTodo()
+            $("#title-update").val("")
+            $("#description-update").val("")
+            $("#due_date-update").val("")
+            $("#status-update").val("")
         })
-        .fail(err => {
-            console.log(err)
+        .fail(err => {     
+            $(".loading").hide()       
             $("#errorUpdate").empty()
-            $("#errorUpdate").append(`<p class="text-danger"> All input must be filled <br> Due date at least is tomorrow </p>`)
+            err.responseJSON.error.forEach( el => {
+                $("#errorUpdate").append(`<p class="text-danger"> ${el} </p>`)
+            })
         })
 }
 
@@ -323,6 +381,8 @@ $(document).ready(function(){
     $("#logout").on("click", function(){
         localStorage.clear()
         landingPage()
+        signOut()
     })
 
 })
+
